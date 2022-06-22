2b9660a2e3348842d45a2e544b273541a964c9f8
Richard Damilola <97083157+Chincoboi@users.noreply.github.com>
2022-06-22 10:06:51 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+var firstName = 'Ogungbile';
+var lastName = 'Richard';
+var middleName = 'Damilola';
+var height = '1.89m';
+var country = 'Nigeria';
+
+let name = firstName + ' ' + middleName + ' ' + lastName;
+
+document.write('Name:' + ' ' +name + '<br>');
+document.write('Height:' + ' ' +height + '<br>');
+document.write('Country:' + ' ' +country);
